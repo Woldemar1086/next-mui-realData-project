1451a11540e6fdffe6764aca118ac7933aae6fb7
Vladimir Khodyrev <khodyrev.v.a@gmail.com>
2026-06-07 21:36:54 +0200

    Remove unused classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({ children }: IChildren) {
       <html
           lang="en"
           data-color-scheme="dark"
-          className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-          <body className="min-h-full flex flex-col">
+          className={`${geistSans.variable} ${geistMono.variable} `}>
+          <body>
             <Providers>
                 <AppShell>
                     {children}
