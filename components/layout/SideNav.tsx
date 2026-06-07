a6fc5d1fc5e0828face7033cd36362243ea0f6d1
Vladimir Khodyrev <khodyrev.v.a@gmail.com>
2026-06-07 21:34:07 +0200

    Add SideNav

---
+++ components/layout/SideNav.tsx
@@ -0,0 +1,40 @@
+import  Drawer from "@mui/material/Drawer";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import {NavigationItems} from "@/components/layout/navigationConfig";
+import Toolbar from "@mui/material/Toolbar";
+
+const DRAWER_WIDTH = 240;
+
+export const SideNav = () =>{
+    const pathname = usePathname();
+    return (
+        <Drawer
+            variant="permanent"
+            sx={{
+                width: DRAWER_WIDTH,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: DRAWER_WIDTH,
+                    boxSizing: 'border-box',
+                },
+            }}>
+            <Toolbar/>
+            <List>
+                {NavigationItems.map(({ title, path, icon: Icon }) => (
+                    <ListItem key={path} disablePadding>
+                        <ListItemButton component={Link} href={path} selected={pathname === path}>
+                            <ListItemIcon><Icon /></ListItemIcon>
+                            <ListItemText primary={title} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    )
+}
---
+++ components/layout/navigationConfig.ts
@@ -0,0 +1,27 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import SettingsIcon from '@mui/icons-material/Settings';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+
+export const NavigationItems = [
+    {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: DashboardIcon
+    },
+    {
+        title: 'Transactions',
+        path: '/transactions',
+        icon: CurrencyExchangeIcon
+    },
+    {
+        title: 'Live feed',
+        path: '/feed',
+        icon: RssFeedIcon
+    },
+    {
+        title: 'Settings',
+        path: '/settings',
+        icon: SettingsIcon
+    },
+]
